87ce2899fe8a0e1910a0dcd659b8a2e9507d5c99
unknown <ahkrell@gmail.com>
2025-02-27 08:41:00 -0500

    Updated AddForm and EditForm pages

--- vite-project/src/pages/AddForm.jsx
+++ vite-project/src/pages/AddForm.jsx
@@ -1,32 +1,38 @@
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function AddForm() {
-  const nav = useNavigate();
-  const [formData, setFormData] = useState({ mech: '' });
+export default function AddForm(){
+    const [formData, setFormData] = useState();
 
-  function handleChange(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      let res = await axios.post('http://localhost:3000/mechs', formData);
-
-      nav('/')
-    } catch (err) {
-      console.error(err);
+    function handleChangeText(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
-  }
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} type='text' name='mech' />
-        <input type='submit' />
-      </form>
-    </>
-  );
+    function handleChangeNumber(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.valueAsNumber });
+    }
+
+    async function handleSubmit(){
+        try{
+          let res = await axios.post('http://localhost:3000/api/mechs', formData);
+        }catch(err){
+          console.error(err);
+        }
+    }
+    
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>ID: </label>
+                <input onChange={handleChangeNumber} type="number" name="id"/><br/>
+                <label>Mech name: </label>
+                <input onChange={handleChangeText} type="text" name="name"/><br/>
+                <label>HP: </label>
+                <input onChange={handleChangeNumber} type="number" name="hp"/><br/>
+                <label>Evasion: </label>
+                <input onChange={handleChangeNumber} type="number" name="evasion"/><br/>
+                <input type="submit"/>
+            </form>
+        </div>
+    )
 }
--- vite-project/src/pages/EditForm.jsx
+++ vite-project/src/pages/EditForm.jsx
@@ -1,33 +1,57 @@
-import axios from "axios";
+import axios from 'axios';
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-export default function EditForm({current}) {
-  const nav = useNavigate();
-  const [formData, setFormData] = useState({ mech: current.mech});
+export default function EditForm(){
+    const [formData, setFormData] = useState();
+    const [currentID, setCurrentID] = useState();
 
-  function handleChange(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-
-      let res = await axios.put(`http://localhost:3000/mechs/${current._id}`, formData)
-
-      nav('/')
-    } catch (err) {
-      console.error(err);
+    function handleChangeID(e) {
+        setCurrentID(e.target.valueAsNumber);
+        setFormData({ ...formData, [e.target.name]: e.target.valueAsNumber });
     }
-  }
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} type='text' name='mechs' value={formData.mechs} />
-        <input type='submit' value='Save Edit' />
-      </form>
-    </>
-  );
+    function handleChangeText(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    function handleChangeNumber(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.valueAsNumber });
+    }
+
+    async function handleSubmit(){
+        try{
+          let res = await axios.patch(`http://localhost:3000/api/mechs/${currentID}`, formData);
+        }catch(err){
+          console.error(err);
+        }
+    }
+
+    async function handleDelete(){
+      try{
+        let res = await axios.delete(`http://localhost:3000/api/mechs/${currentID}`);
+      }catch(err){
+        console.error(err);
+      }
+  }
+    
+    return(
+        <div>
+            <form>
+                <label>Enter ID: </label>
+                <input onChange={handleChangeID} type="number" name="id"/><br/>
+            </form>
+            <form onSubmit={handleSubmit}>
+                <label>Mech name: </label>
+                <input onChange={handleChangeText} type="text" name="name"/><br/>
+                <label>HP: </label>
+                <input onChange={handleChangeNumber} type="number" name="hp"/><br/>
+                <label>Evasion: </label>
+                <input onChange={handleChangeNumber} type="number" name="evasion"/><br/>
+                <input type="submit" value="Update"/>
+            </form>
+            <form onSubmit={handleDelete}>
+                <input type="submit" value="Delete"/>
+            </form>
+        </div>
+    )
 }
